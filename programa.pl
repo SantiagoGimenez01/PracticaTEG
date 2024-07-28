12726fcad341dcbb469f01f73427179707d73b11
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-07-27 21:43:02 -0300

    Avance TEG 2

--- programa.pl
+++ programa.pl
@@ -1,3 +1,5 @@
+%%                                              PRACTICA 1
+/*
 % Se cumple para los jugadores.
 % jugador(Jugador).
 % Ejemplo:
@@ -105,3 +107,103 @@ puedeAtacar(Jugador, PaisAtacado):-
     ocupa(Jugador, PaisPropio),   
     not((aliados(Jugador, OtroJugador), ocupa(OtroJugador, PaisAtacado))).
 
+*/
+
+%%                                              PRACTICA 2
+continente(americaDelSur).
+continente(americaDelNorte).
+continente(asia).
+continente(oceania).
+
+estaEn(americaDelSur, argentina).
+estaEn(americaDelSur, brasil).
+estaEn(americaDelSur, chile).
+estaEn(americaDelSur, uruguay).
+estaEn(americaDelNorte, alaska).
+estaEn(americaDelNorte, yukon).
+estaEn(americaDelNorte, canada).
+estaEn(americaDelNorte, oregon).
+estaEn(asia, kamtchatka).
+estaEn(asia, china).
+estaEn(asia, siberia).
+estaEn(asia, japon).
+estaEn(oceania,australia).
+estaEn(oceania,sumatra).
+estaEn(oceania,java).
+estaEn(oceania,borneo).
+
+jugador(amarillo).
+jugador(magenta).
+jugador(negro).
+jugador(blanco).
+
+aliados(X,Y):- alianza(X,Y).
+aliados(X,Y):- alianza(Y,X).
+alianza(amarillo,magenta).
+
+%el numero son los ejercitos
+ocupa(argentina, magenta, 5).
+ocupa(chile, negro, 3).
+ocupa(brasil, amarillo, 8).
+ocupa(uruguay, magenta, 5).
+ocupa(alaska, amarillo, 7).
+ocupa(yukon, amarillo, 1).
+ocupa(canada, amarillo, 10).
+ocupa(oregon, amarillo, 5).
+ocupa(kamtchatka, negro, 6).
+ocupa(china, amarillo, 2).
+ocupa(siberia, amarillo, 5).
+ocupa(japon, amarillo, 7).
+ocupa(australia, negro, 8).
+ocupa(sumatra, negro, 3).
+ocupa(java, negro, 4).
+ocupa(borneo, negro, 1).
+
+% Usar este para saber si son limitrofes ya que es una relacion simetrica
+sonLimitrofes(X, Y) :- limitrofes(X, Y).
+sonLimitrofes(X, Y) :- limitrofes(Y, X).
+
+limitrofes(argentina,brasil).
+limitrofes(argentina,chile).
+limitrofes(argentina,uruguay).
+limitrofes(uruguay,brasil).
+limitrofes(alaska,kamtchatka).
+limitrofes(alaska,yukon).
+limitrofes(canada,yukon).
+limitrofes(alaska,oregon).
+limitrofes(canada,oregon).
+limitrofes(siberia,kamtchatka).
+limitrofes(siberia,china).
+limitrofes(china,kamtchatka).
+limitrofes(japon,china).
+limitrofes(japon,kamtchatka).
+limitrofes(australia,sumatra).
+limitrofes(australia,java).
+limitrofes(australia,borneo).
+limitrofes(australia,chile).
+
+%% Parte A
+
+loLiquidaron(Jugador):-
+    jugador(Jugador),
+    not(ocupa(_, Jugador, _)).
+
+ocupaContinente(Jugador, Continente):-
+    jugador(Jugador), continente(Continente),
+    forall(estaEn(Continente, Pais), ocupa(Pais, Jugador, _)).
+
+seAtrinchero(Jugador):-
+    jugador(Jugador), continente(Continente),
+    forall(ocupa(Pais, Jugador, _), estaEn(Continente, Pais)).  
+
+%% Parte B
+
+puedeConquistar(Jugador, Continente):-
+    jugador(Jugador), continente(Continente), not(ocupaContinente(Jugador, Continente)),
+    forall((estaEn(Continente, Pais), not(ocupa(Pais, Jugador, _))), puedeAtacar(Jugador, Pais)).
+
+puedeAtacar(Jugador, Pais):-
+    ocupa(SuPais, Jugador, _),
+    sonLimitrofes(SuPais, Pais),
+    ocupa(Pais, OtroJugador, _),
+    not(aliados(Jugador, OtroJugador)).
